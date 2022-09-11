1b81b2dd84c950fe09f645484deb5fcaafa002fa
yangchaoxu <yangchaoxu@bytedance.com>
2022-09-12 00:20:27 +0800

    feat: init

---
+++ src/view/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Project } from "./project";
+
+function App() {
+  return (
+    <div className="App">
+      <Project></Project>
+    </div>
+  );
+}
+
+export default App;
