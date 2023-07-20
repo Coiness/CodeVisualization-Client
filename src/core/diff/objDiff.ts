4477e560ffade195d5d8b30a3c822fc43ce94af1
60rzvvbj <1808078515@qq.com>
2023-07-21 00:46:36 +0800

    feat: 拆分 changeSet 类型

--- src/core/diff/objDiff.ts
+++ src/core/diff/objDiff.ts
@@ -6,13 +6,29 @@ import { initPathDfs } from "../undo";
 
 export const modelChange = new Subject<CommonModel>();
 
-// todo any 治理 & 拆分 ChangeSet 类型
-export type ChangeSet = {
+interface ChangeBase {
   modelPath: string; // 对应逻辑层 model path
-  t: "u" | "d" | "c"; // 操作类型
   p: string; // 操作路径 path
-  c: any[]; // 具体操作
-}[];
+}
+
+interface ChangeCreate extends ChangeBase {
+  t: "c";
+  c: [unknown];
+}
+
+interface ChangeDelete extends ChangeBase {
+  t: "d";
+  c: [unknown];
+}
+
+interface ChangeUpdate extends ChangeBase {
+  t: "u";
+  c: [unknown, unknown];
+}
+
+type Change = ChangeCreate | ChangeDelete | ChangeUpdate;
+
+export type ChangeSet = Change[];
 
 export function getModelByPath(obj: Obj, path: string): CommonModel {
   const res = get(obj, path);
@@ -52,15 +68,24 @@ export function doInvertedChange(obj: Obj, cs: ChangeSet) {
 
 function getInvertedChangeSet(cs: ChangeSet) {
   cs = cloneDeep(cs);
-  cs.forEach((change) => {
+  cs = cs.map((change: Change): Change => {
     if (change.t === "c") {
-      change.t = "d";
+      return {
+        ...change,
+        t: "d",
+      };
     } else if (change.t === "d") {
-      change.t = "c";
+      return {
+        ...change,
+        t: "c",
+      };
     } else if (change.t === "u") {
       let temp = change.c[0];
       change.c[0] = change.c[1];
       change.c[1] = temp;
+      return { ...change };
+    } else {
+      throw new Error("getInvertedChangeSet: change type error");
     }
   });
   cs.reverse(); // CS 中可能会有先后依赖关系，所以获取反向 CS 后需要将其反转
